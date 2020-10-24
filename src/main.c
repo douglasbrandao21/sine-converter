04fc7b9c16871ff1ec5f0036d09aef24eaaceda7
douglasbrandao21 <douglasbrando5@gmail.com>
2020-10-24 11:15:28 -0300

    reading the trigonometric table

--- src/main.c
+++ src/main.c
@@ -14,28 +14,44 @@ int getNumberOfSins() {
   int numberOfSins = 0;
   float aux = 0;
 
-  while(fscanf(sinsFile, "%f", &aux) == 1) {
+  while(fscanf(sinsFile, "%f", &aux) == 1)
     numberOfSins++;
-  }
 
   return numberOfSins;
 }
 
-void getSins(float *sin, int numberOfSins) {
-  FILE* sinsFile = fopen("../files/seno.txt", "r");
+void filesToArray(float *array, int numberOfElements) {
+  FILE* file = fopen("../files/seno.txt", "r");
 
   int index;
 
-  for(index = 0; index < numberOfSins; index++)
-    fscanf(sinsFile, "%f", &sin[index]);
+  for(index = 0; index < numberOfElements; index++)
+    fscanf(file, "%f", &array[index]);
+}
+
+void getTrigonometricTable(float *sin, float *cos, int *degrees) {
+  FILE* trigonometricTable = fopen("../files/trigo.txt", "r");
+
+  for(int index = 0; index <= 90; index++) {
+    fscanf(trigonometricTable, "%d", &degrees[index]);
+    fscanf(trigonometricTable, "%f", &sin[index]);
+    fscanf(trigonometricTable, "%f", &cos[index]);
+  }
 }
 
 int main() {
   int numberOfSins = getNumberOfSins();
 
-  float sin[numberOfSins];
+  float inputSins[numberOfSins];
 
-  getSins(&sin[0], numberOfSins);
+  int degrees[91];
+
+  float sin[91];
+  float cos[91];
+
+  filesToArray(&inputSins[0], numberOfSins);
+
+  getTrigonometricTable(&sin[0], &cos[0], &degrees[0]);
 
   return 0;
 }
